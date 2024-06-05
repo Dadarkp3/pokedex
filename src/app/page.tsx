65fd08458e79702405b3ca43becf7934b26aa203
Daiane Mascarenhas <adm.daianesilva@gmail.com>
2024-06-05 18:18:40 -0300

    feat: PK02 - Add lint, prettier, husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
         </p>
         <div>
           <a
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             By{' '}
             <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
+              src="/vercel.svg"
+              alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
@@ -32,10 +32,10 @@ export default function Home() {
 
       <div className={styles.grid}>
         <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <h2>
             Docs <span>-&gt;</span>
@@ -44,10 +44,10 @@ export default function Home() {
         </a>
 
         <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <h2>
             Learn <span>-&gt;</span>
@@ -56,10 +56,10 @@ export default function Home() {
         </a>
 
         <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <h2>
             Templates <span>-&gt;</span>
@@ -68,10 +68,10 @@ export default function Home() {
         </a>
 
         <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <h2>
             Deploy <span>-&gt;</span>
