372e8fe8e34f86c5d50ca45870f3f542732b76e2
Daiane Mascarenhas <adm.daianesilva@gmail.com>
2024-06-06 19:32:59 -0300

    feat: pk14 - add the mnodels and interfaces

---
+++ src/models/Pokemon..ts
@@ -0,0 +1,31 @@
+export interface PokemonResponseList {
+  name: string;
+  url: string;
+}
+
+export interface PokemonPage {
+  count: number;
+  results: PokemonResponseList[];
+  next: string | null;
+  previous: string | null;
+}
+
+export interface PokemonType {
+  type: {
+    name: string;
+  };
+}
+
+export interface Pokemon {
+  name: string;
+  types: PokemonType[];
+  weight: number;
+  height: number;
+  sprites: {
+    other: {
+      'official-artwork': {
+        front_default: string;
+      };
+    };
+  };
+}
