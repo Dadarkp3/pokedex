5581c077213f0054290f37ec106e1dcdf9214719
Daiane Mascarenhas <adm.daianesilva@gmail.com>
2024-06-10 21:38:40 -0300

    fix: pk-30 remove unecessary code

--- src/lib/utils/__tests__/utils.spec.ts
+++ src/lib/utils/__tests__/utils.spec.ts
@@ -1,42 +1,8 @@
 import { ChangeEvent, KeyboardEvent } from 'react';
 
-import { pokemonMock } from 'lib/mocks/PokemonMock';
-import { fetchPokemonMock } from 'lib/utils/hooksUtils';
-
 import { handleInputChange, handleKeyUp } from '../searchUtils';
 
 describe('utils', () => {
-  describe('hooksUtils', () => {
-    test('should resolve with pokemon data for matching name (case-insensitive)', async () => {
-      const name = 'dItTo';
-      const pokemon = await fetchPokemonMock(name);
-
-      expect(pokemon).toEqual(pokemonMock);
-    });
-
-    test('should reject with error for non-matching name', async () => {
-      const name = 'Bulbasaur';
-
-      try {
-        await fetchPokemonMock(name);
-        fail('Expected error to be thrown');
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (error: any) {
-        expect(error.message).toBe('Pokémon não encontrado'); // Might need additional checks
-      }
-    });
-
-    // Optional test for empty name handling (adjust as needed)
-    test('should handle empty name', async () => {
-      try {
-        await fetchPokemonMock('');
-        fail('Expected error to be thrown'); // Adjust assertion if needed
-      } catch (error) {
-        expect(error).toBeInstanceOf(Error); // Generic error assertion
-      }
-    });
-  });
-
   describe('handleInputChange', () => {
     it('should update the search term', () => {
       const setSearchTerm = jest.fn();
